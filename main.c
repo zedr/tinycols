dfa95d5268b57963130eaff1b2d13252260aed59
zedr <zedr@zedr.com>
2022-04-28 23:08:56 +0200

    Display points

--- main.c
+++ main.c
@@ -81,6 +81,10 @@ draw_frames (game_t *game)
 				   start_y - 1,
 				   start_x + BOARD_COLS + 3,
 				   start_y + PIECE_SIZE);
+		draw_rect (start_x + BOARD_COLS + 1,
+				   start_y + 4,
+				   start_x + BOARD_COLS + 9,
+				   start_y + 6);
 	}
 	refresh();
 }
@@ -109,8 +113,8 @@ process_keys (game_t *game)
 			break;
 		case KEY_DOWN:
 			if (move_command (MOVE_DOWN, game, &erase_piece)) {
-                game->score += (game->level + 1);
-            }
+				game->score += (game->level + 1);
+			}
 			break;
 		case KEY_UP:
 			shift_command (SHIFT_UP, game->current, &erase_piece);
@@ -121,25 +125,44 @@ process_keys (game_t *game)
 }
 
 static void
-display_deleted(results_t *results)
+display_deleted (results_t *results)
 {
-    int do_pause = 0;
-    div_t d;
+	int do_pause = 0;
+	div_t d;
 
-    for (int i = 0; i < BOARD_CELLS; i++) {
-        if (results->targets[i] > 0) {
-            do_pause = 1;
-            d = div(i, BOARD_COLS);
-            mvaddch(start_y + d.quot,
-                    start_x + d.rem,
-                    '*');
-        }
-    }
+	for (int i = 0; i < BOARD_CELLS; i++) {
+		if (results->targets[i] > 0) {
+			do_pause = 1;
+			d = div (i, BOARD_COLS);
+			mvaddch(start_y + d.quot,
+					start_x + d.rem,
+					'*');
+		}
+	}
 
-    if (do_pause) {
-        refresh();
-        usleep(250000);
-    }
+	if (do_pause) {
+		refresh();
+		usleep (250000);
+	}
+}
+
+static void
+display_points (unsigned long points)
+{
+	if (points) {
+		mvprintw (start_y + 5,
+				  start_x + 8,
+				  "+%d",
+				  points);
+	}
+
+}
+static void
+hide_points (void)
+{
+	mvprintw (start_y + 5,
+			  start_x + 8,
+			  "     ");
 }
 
 static int
@@ -147,7 +170,8 @@ game_tick (game_t *game)
 {
 	unsigned long ts = time (NULL);
 	results_t results;
-    int chain = 0;
+	int chain = 0;
+	unsigned long points;
 	int deleted;
 
 	if (ts > last_ts) {
@@ -158,12 +182,15 @@ game_tick (game_t *game)
 				init_results (&results);
 				do {
 					find_clusters (game->board, &results);
-                    display_deleted(&results);
-                    game->score += calc_points(&results, game->level, chain);
+					points = calc_points (&results, game->level, chain);
+					display_points (points);
+					display_deleted (&results);
+					hide_points ();
+					game->score += points;
 					deleted = delete_clusters (game->board, &results);
 					if (deleted) {
 						apply_gravity (game->board, &move_column);
-                        chain++;
+						chain++;
 					} else {
 						break;
 					}
@@ -176,7 +203,7 @@ game_tick (game_t *game)
 		}
 	}
 
-	usleep(100);
+	usleep (100);
 
 	return 1;
 }
@@ -184,12 +211,12 @@ game_tick (game_t *game)
 static void
 draw_debug (game_t *game)
 {
-    mvprintw (1, 1, "score: \t%ld ", game->score);
-    if (debug) {
-        mvprintw (2, 1, "piece coords: \t%d, %d ",
-                  game->current->x, game->current->y);
-        mvprintw (3, 1, "st: \t%d, %d ", start_x, start_y);
-    }
+	mvprintw (1, 1, "score: \t%ld ", game->score);
+	if (debug) {
+		mvprintw (2, 1, "piece coords: \t%d, %d ",
+				  game->current->x, game->current->y);
+		mvprintw (3, 1, "st: \t%d, %d ", start_x, start_y);
+	}
 }
 
 static void
@@ -204,7 +231,7 @@ play_game (void)
 	do {
 		draw_board (game->board);
 		draw_piece (game->current);
-        draw_piece (game->next);
+		draw_piece (game->next);
 		draw_debug (game);
 		process_keys (game);
 		refresh();
